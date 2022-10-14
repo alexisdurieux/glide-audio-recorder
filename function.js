3f89f5dea0f1228c74681296430c58f9b7f0c930
Alexis Durieux <alexis.durieux.pro@gmail.com>
2022-10-14 10:39:04 +0200

    Initial commit

---
+++ function.js
@@ -0,0 +1,18 @@
+window.function = function (uploadUrl) {
+
+    ht = `
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="UTF-8">
+            <title>Audio Recorder</title>
+        </head>
+        <body>
+         <button type="button" value="false" class="recording-btn">Record</button> 
+         <button type="button" class="send-btn">Send</button> 
+        </body>
+    </html>
+    `
+  
+  return ht
+}
